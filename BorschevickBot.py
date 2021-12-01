5ff930076d616dd20528ad78fcfb0307e2d49687
yamaha3212 <67462069+yamaha3212@users.noreply.github.com>
2021-12-02 00:50:45 +0300

    Face recognition added
    Now bot can locate faces and send information about this

--- BorschevickBot.py
+++ BorschevickBot.py
@@ -2,8 +2,23 @@ import telebot
 import os
 import inspect
 import sys
+from PIL import Image
+import face_recognition
+import numpy as np
+from io import BytesIO
 
 bot = telebot.TeleBot("2147259007:AAEVsREyP6oCv5-YCxIyk45DyoTtW-4ui1s", parse_mode=None)
+
+
+def show_faces(face_locations, image, message):
+    for face_location in face_locations:
+        top, right, bottom, left = face_location
+
+        face_image = image[top:bottom, left:right]
+        pil_image = Image.fromarray(face_image)
+        pil_image.save('FindedFace.jpg')
+        photo = open('FindedFace.jpg', 'rb')
+        bot.send_photo(message.chat.id, photo, 'Лицо с этой фотографии:', message.id)
 
 
 def get_my_dir(follow_symlinks=True):
@@ -28,11 +43,19 @@ def handle_docs_photo(message):
 
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
+        bot.reply_to(message, "Сейчас поищу лица на этой фотографии...")
+        image = np.array(Image.open(BytesIO(downloaded_file)))
+        face_locations = face_recognition.face_locations(image)
 
-        src = get_my_dir() + os.path.sep + file_info.file_path
-        with open(src, 'wb') as new_file:
-            new_file.write(downloaded_file)
-        bot.reply_to(message, "Фото добавлено")
+        if len(face_locations) > 0:
+            src = get_my_dir() + os.path.sep + file_info.file_path
+            with open(src, 'wb') as new_file:
+                new_file.write(downloaded_file)
+            bot.reply_to(message, "Я вижу людей на этой фотографии, сохраню ее на сервере для дальнейших исследований")
+            show_faces(face_locations, image, message)
+
+        else:
+            bot.reply_to(message, "Кажется, на этой фотографии нет лиц... ну или человеческих лиц")
 
     except Exception as e:
         bot.reply_to(message, str(e))
