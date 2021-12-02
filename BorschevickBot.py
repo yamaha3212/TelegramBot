4e44dfadb5e4189c7ef0d6dea1370625c5f4285f
yamaha3212 <67462069+yamaha3212@users.noreply.github.com>
2021-12-02 03:01:27 +0300

    Voice downloading added

--- BorschevickBot.py
+++ BorschevickBot.py
@@ -6,9 +6,21 @@ from PIL import Image
 import face_recognition
 import numpy as np
 from io import BytesIO
+import random
+from time import time
+import librosa
+import io
+import soundfile as sf
+from scipy.io.wavfile import read, write
 
 bot = telebot.TeleBot("2147259007:AAEVsREyP6oCv5-YCxIyk45DyoTtW-4ui1s", parse_mode=None)
-
+ORDINATA = ['Ars longa, vita brevis.',
+            'Per aspera ad astra.',
+            'Usus est optimus magister.',
+            'Contra malum mortis non est medicamentum in hortis.',
+            'Alea jacta est.',
+            'Non ducor, duco.',
+            'Etiam si omnes, ego non.']
 
 def show_faces(face_locations, image, message):
     for face_location in face_locations:
@@ -38,9 +50,8 @@ except FileExistsError:
 
 
 @bot.message_handler(content_types=['photo'])
-def handle_docs_photo(message):
+def handle_photo(message):
     try:
-
         file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
         downloaded_file = bot.download_file(file_info.file_path)
         bot.reply_to(message, "Сейчас поищу лица на этой фотографии...")
@@ -57,6 +68,28 @@ def handle_docs_photo(message):
         else:
             bot.reply_to(message, "Кажется, на этой фотографии нет лиц... ну или человеческих лиц")
 
+    except Exception as e:
+        bot.reply_to(message, str(e))
+
+
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    try:
+        bot.reply_to(message, "Сохраню это на сервере и попрошу кого-нибудь перевести, ведь я понимаю только латинский язык. " + ORDINATA[random.randint(0, len(ORDINATA) - 1)])
+        file_info = bot.get_file(message.voice.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+
+        #FIXME
+        #signal_gm = np.frombuffer(downloaded_file, dtype='float64')
+        #sf.write('new_file.wav', signal_gm, 16000)
+        #FIXME
+
+        try:
+            os.mkdir(get_my_dir() + os.path.sep + str(message.chat.id))
+        except FileExistsError:
+            pass
+        with open(f'{message.chat.id}_{int(time())}.wav', 'wb') as new_file:
+            new_file.write(downloaded_file)
     except Exception as e:
         bot.reply_to(message, str(e))
 
